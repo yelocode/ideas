6e1dcc8bca5f195067d63a6b57f6efd8e3e68099
Matin <matin.mn@hotmail.com>
2023-07-14 11:51:09 +0800

    init project

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index(){
+        return view('profile');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [DashboardController::class , 'index'] );
+
+
+Route::get('/profile', [ProfileController::class , 'index'] );
