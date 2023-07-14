8bfb874f0db60fbe656b948f002845f5f4daa6db
Matin <matin.mn@hotmail.com>
2023-07-15 05:48:49 +0800

    reset to base project files

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,8 +6,9 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-   
-    public function index(){
-        return view('welcome');
+
+    public function index()
+    {
+        return view('dashboard');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -16,6 +17,3 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [DashboardController::class , 'index'] );
-
-
-Route::get('/profile', [ProfileController::class , 'index'] );
