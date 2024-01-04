6fb91a66df3f85bcd261c0a75346ed44a3964280
Matin <matin.mn@hotmail.com>
2024-01-05 06:47:50 +0800

    ep 46 | caching

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Foundation\Application;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -26,11 +27,15 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFive();
 
+        $topUsers = cache()->remember('topUsers', now()->addMinutes(5), function () {
+            return User::withCount('ideas')
+                ->orderBy('ideas_count', 'DESC')
+                ->take(10)->get();
+        });
+
         View::share(
             'topUsers',
-            User::withCount('ideas')
-                ->orderBy('ideas_count', 'DESC')
-                ->take(5)->get()
+            $topUsers
         );
     }
 }
